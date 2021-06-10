d8cd1143257e83658046120432859bf89dd9f642
ScienceCrafter <42043052+ScienceCrafter@users.noreply.github.com>
2021-06-09 22:48:06 -0400

    more speeds!!
    added yeetbullet and purgatory modes
    (stupidly fast, stupidly slow, do not play these if you want to have fun)

--- speeds.js
+++ speeds.js
@@ -52,12 +52,26 @@ window.snake.speeds = function() {
       img.class = 'DqMRee SsAred';
       document.querySelector('#speed').appendChild(img);
 
+      img = new Image;
+      img.src = 'https://i.postimg.cc/NMCVrwR2/aaaaaaaaa.png';
+      img.width = 47;
+      img.height = 47;
+      img.class = 'DqMRee SsAred';
+      document.querySelector('#speed').appendChild(img);
+
+      img = new Image;
+      img.src = 'https://i.postimg.cc/mDHJv50v/eternal.png';
+      img.width = 47;
+      img.height = 47;
+      img.class = 'DqMRee SsAred';
+      document.querySelector('#speed').appendChild(img);
+
       eval(
         code.match(
           /[a-zA-Z0-9_$]{1,6}=function\(a\){switch\(a\.[a-zA-Z0-9_$]{1,6}\){case 1:return\.66[^}]*?1}}/
         )[0].replace(
           '1.33;',
-          '1.33;case 3:return 1.85;case 4:return 0.45;case 5:return 18.5;case 6:return 0.35;case 7:return 0.25;case 8:return 0.15;'
+          '1.33;case 3:return 1.85;case 4:return 0.45;case 5:return 18.5;case 6:return 0.35;case 7:return 0.25;case 8:return 0.15;case 9:return 0.05;case 10:return 26640;'
         )
       );
     };
